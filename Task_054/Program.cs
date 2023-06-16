7192eece33496c0b824fe2fcd5dffd8cc04fdd06
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-16 20:29:37 +0300

    problem not solved using methods

---
+++ Task_054/Program.cs
@@ -0,0 +1,14 @@
+﻿//Create two-dimensional array.
+//Sort the elements in each row in descending order/ - in descending order - в порядке убывания.
+                                                   //- each - каждый.
+
+using System;
+using static System.Console;
+
+/*for (int i = 0; i < array.GetLength(0); i++)
+{
+    Array.Sort(array, i * array.GetLength(1), array.GetLength(1), new Comparer<int>((a, b) => {
+        return b.CompareTo(a);
+    }));
+}*/
+
